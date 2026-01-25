3531bc95f4e5afd53aeb6e26b49ca8f84bb058f1
tlsappointements-oss <tls.appointements@gmail.com>
2026-01-25 01:53:16 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -6,11 +7,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyDnp4fC2_cEw04ydtWOwYgVzRUsqScufFs",
   authDomain: "cars-website-558c0.firebaseapp.com",
   projectId: "cars-website-558c0",
-  storageBucket: "cars-website-558c0.appspot.com",
+  storageBucket: "cars-website-558c0.firebasestorage.app",
   messagingSenderId: "142475379783",
-  appId: "1:142475379783:web:2849d07fb6eb8da4715d62"
+  appId: "1:142475379783:web:2849d07fb6eb8da4715d62",
+  measurementId: "G-53ZQY6XJJR"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export for your other files
 export const auth = getAuth(app);
 export const db = getFirestore(app);
